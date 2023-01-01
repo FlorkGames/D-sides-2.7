b1624983a644f4876841a1d5f677647b5fd6057b
FlorkGames <105853494+FlorkGames@users.noreply.github.com>
2023-01-01 02:12:28 -0300

    Add files via upload

---
+++ assets/preload/scripts/script6.lua
@@ -0,0 +1,5 @@
+function onCreate() 
+makeLuaText('watermark',"Collab Apk With ShyrxZ", 0, 2, 688); -- You can edit the created by Prisma Text just dont change anything else
+    setTextSize('watermark', 50);
+    addLuaText('watermark');
+end
